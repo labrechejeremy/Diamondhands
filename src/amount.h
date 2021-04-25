0a0ff1f6064921ce980bdb9582f82800455b0857
labrechejeremy <labreche_jeremy@outlook.com>
2021-04-24 19:01:02 -0700

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_AMOUNT_H
-#define BITCOIN_AMOUNT_H
+#ifndef DIAMONDHANDS_AMOUNT_H
+#define DIAMONDHANDS_AMOUNT_H
 
 #include <stdint.h>
 
@@ -22,7 +22,7 @@ static const CAmount COIN = 100000000;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 21000000 * COIN;
+static const CAmount MAX_MONEY = 10000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
-#endif //  BITCOIN_AMOUNT_H
+#endif //  DIAMONDHANDS_AMOUNT_H
